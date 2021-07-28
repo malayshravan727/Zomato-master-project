4ae27e13a6da7dfbab39294883dff8b038b4c453
malayshravan727 <malayshravan727@gmail.com>
2021-07-28 21:07:13 +0530

    add google auth

--- server/API/Auth/index.js
+++ server/API/Auth/index.js
@@ -2,6 +2,7 @@
 import express from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import passport from "passport";
 
 
 //Models
@@ -44,5 +45,35 @@ Router.post("/signup", async (req, res) => {
       return res.status(500).json({ error: error.message });
     }
   });
+
+/*
+Route     /google
+Des       Google Signin
+Params    none
+Access    Public
+Method    GET  
+*/
+Router.get("/google", passport.authenticate("google", {
+    scope: [
+      "https://www.googleapis.com/auth/userinfo.profile",
+      "https://www.googleapis.com/auth/userinfo.email",
+    ],
+  })
+);
+
+/*
+Route     /google/callback
+Des       Google Signin Callback
+Params    none
+Access    Public
+Method    GET  
+*/
+Router.get(
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: "/" }),
+  (req, res) => {
+    return res.json({ token: req.session.passport.user.token });
+  }
+);
   
 export default Router;
