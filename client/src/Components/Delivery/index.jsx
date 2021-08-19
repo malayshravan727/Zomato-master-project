2b182e7a4d5461bbf87bdf0846dec80982c54fa8
malayshravan727 <malayshravan727@gmail.com>
2021-08-19 19:40:39 +0530

    add food category carousel

---
+++ client/src/Components/Delivery/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+// Components
+import DeliveryCarousal from "./DeliveryCarousal";
+
+const Delivery = () => {
+  return (
+    <>
+      <DeliveryCarousal />
+    </>
+  );
+};
+
+
+export default Delivery;
