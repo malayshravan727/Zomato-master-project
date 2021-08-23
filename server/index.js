2f656c8f5aa0d071aac58b292fe3f64dd4f87040
malayshravan727 <malayshravan727@gmail.com>
2021-08-23 09:38:26 +0530

    add mail service

--- server/index.js
+++ server/index.js
@@ -19,6 +19,8 @@ import Image from "./API/Image";
 import Order from "./API/Orders";
 import Reviews from "./API/Reviews";
 import User from "./API/User";
+import Menu from "./API/Menu";
+import MailService from "./API/Mail";
 
 // Database connection
 import ConnectDB from "./database/connection";
@@ -45,6 +47,8 @@ zomato.use("/image", Image);
 zomato.use("/order", Order);
 zomato.use("/reviews", Reviews);
 zomato.use("/user", User);
+zomato.use("/menu", Menu);
+zomato.use("/mail", MailService);
 
 zomato.get("/" , (req, res) => res.json({ message: "Setup success" }));
 
